6a3971f0316ec3f9923a9e53da9ae60d63c8280a
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2025-01-23 21:50:01 +0530

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,13 @@
+from vars import*
+
+
+class Config:
+    API_ID = API_ID  # Replace with your API ID
+    API_HASH = f"{API_HASH}"  # Replace with your API Hash
+    STRING_SESSION = f"{ELITE_SESSION}"
+    BOT_PREFIX = f"{ELITE_BOT_PREFIX}"
+    BOT_NAME = "Cipher Elite"
+    SUDO_USERS = SUDO_USERS  # Add your user ID here
+    LOG_CHAT_ID = LOG_CHAT_ID  # Add your log channel/group ID
+    BOT_TOKEN = f"{BOT_TOKEN}"
+    TG_BOT_USERNAME = f"{ELITE_BOT_USERNAME}"
