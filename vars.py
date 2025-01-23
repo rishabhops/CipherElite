aad5b8cbd0d0f5ad9815a50a3b7a4261c8508655
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2025-01-23 21:55:14 +0530

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,8 @@
+API_ID = 10248430  # Replace with your API ID
+API_HASH = "42396a6ff14a569b9d59931643897d0d"  # Replace with your API Hash
+ELITE_SESSION = "your telethon session"
+ELITE_BOT_PREFIX = "."
+SUDO_USERS = []  # Add your user ID here
+LOG_CHAT_ID = -1234  # Add your log channel/group ID
+BOT_TOKEN = "" #your bot token
+ELITE_BOT_USERNAME = "" # your bot username 
