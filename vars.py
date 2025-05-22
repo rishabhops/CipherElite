9f7ca66781feb798f5ce4c1a39753555d02728de
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2025-05-22 11:51:05 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,14 +1,17 @@
 # Core Configuration
 API_ID = 10248430
 API_HASH = "42396a6ff14a569b9d59931643897d0d"
-ELITE_SESSION = "your elite session" #make elite session from this bot
+# Create elite session from this bot @elite_session_maker_bot
+ELITE_SESSION = "your elite session"
 # Bot Settings
 ELITE_BOT_PREFIX = "."
-BOT_TOKEN = "your bot token"
-ELITE_BOT_USERNAME = "@cipher_elite_bot" #your bot username 
+# Make a bot from @BotFather and paste the bot token here.
+BOT_TOKEN = "YOUR BOT_TOKEN"
+# Enter your bot username with @
+ELITE_BOT_USERNAME = "@BOT_USERNAME" #your bot username 
 
 # Access Control
-SUDO_USERS = [6167484733, 5470956337] #replace with your ids
+SUDO_USERS = [5470956337] #replace with your ids
 LOG_CHAT_ID = #your logger group id with -100
 
 PMPERMIT_PIC = "https://files.catbox.moe/r7fem2.jpeg"
