bef5dbb3a3b4776ffae2d16fe3b251aa1ef61e71
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2025-07-02 11:09:35 +0530

    Update logs.py

--- plugins/logs.py
+++ plugins/logs.py
@@ -6,7 +6,7 @@ import logging
 from functools import wraps
 
 from utils.utils import CipherElite
-from bot.utils import rishabh, add_handler
+from utils.utils import rishabh, add_handler
 
 # Configure logging
 logging.basicConfig(
